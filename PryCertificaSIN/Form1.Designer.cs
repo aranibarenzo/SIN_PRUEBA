0e4e4caf782eaf53ed6e1062bae1744e345a7f24
enzo aranibar <enzoaranibar@gmail.com>
2022-11-19 22:59:17 -0400

    SIN

---
+++ PryCertificaSIN/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PryCertificaSIN
+{
+    partial class frmMainSIN
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btConectar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btConectar
+            // 
+            this.btConectar.Location = new System.Drawing.Point(280, 192);
+            this.btConectar.Name = "btConectar";
+            this.btConectar.Size = new System.Drawing.Size(527, 62);
+            this.btConectar.TabIndex = 0;
+            this.btConectar.Text = "VERIFICAR CONEXION";
+            this.btConectar.UseVisualStyleBackColor = true;
+            this.btConectar.Click += new System.EventHandler(this.btConectar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(280, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(527, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 358);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(527, 62);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btSalir
+            // 
+            this.btSalir.Location = new System.Drawing.Point(943, 610);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(75, 23);
+            this.btSalir.TabIndex = 3;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // frmMainSIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 660);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btConectar);
+            this.Name = "frmMainSIN";
+            this.Text = "MAIN CERITFICAR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConectar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btSalir;
+    }
+}
+
---
+++ PryCertificaSIN/Form1.cs
@@ -0,0 +1,72 @@
+﻿using PryCertificaSIN.ServiceFacturacionCodigosSIN;
+using PryCertificaSIN.Utiles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Channels;
+
+namespace PryCertificaSIN
+{
+    public partial class frmMainSIN : Form
+    {
+        public frmMainSIN()
+        {
+            InitializeComponent();
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void btConectar_Click(object sender, EventArgs e)
+        {
+            string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJFbnpvIiwiY29kaWdvU2lzdGVtYSI6IjcyNDE4NDAwQzFFQzI4QjZFODRFMTJGIiwibml0IjoiSDRzSUFBQUFBQUFBQURPMk1EQzB0REF6TURRSEFHNkxrVU1LQUFBQSIsImlkIjo2MjAwNDUsImV4cCI6MTY3MjM1ODQwMCwiaWF0IjoxNjY0ODAzMTMyLCJuaXREZWxlZ2FkbyI6MzgwMTk4NjAxNywic3Vic2lzdGVtYSI6IlNGRSJ9._kO93DpE2118YqOZ8k8ZB8NYU7w5ZjYq5MjY62mP24Rh-7btKca6fGBhwSJWadoNmbSJwLuoLlSWR1-49IP8aA";
+            //string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJFbnpvIiwiY29kaWdvU2lzdGVtYSI6IjZEMjQ5Njk0MEFCMzk0QzU3MERBMTJGIiwibml0IjoiSDRzSUFBQUFBQUFBQURPMk1EQzB0REF6TURRSEFHNkxrVU1LQUFBQSIsImlkIjo2MjAwNDUsImV4cCI6MTY0NDUzNzYwMCwiaWF0IjoxNjQ0MTUxMzMyLCJuaXREZWxlZ2FkbyI6MzgwMTk4NjAxNywic3Vic2lzdGVtYSI6IlNGRSJ9.QkqgBdnu3El3ktPg7l05_O0T-P4gkzieiJrj3hHZ87jKZ8uyqySbEOlCXxDnIjflGgI1maTwFlivKQLi6vtbTg";
+            string endpointAddress = "https://pilotosiatservicios.impuestos.gob.bo/v2/FacturacionCodigos?wsdl";//"https://pilotosiatservicios.impuestos.gob.bo/v2/FacturacionOperaciones?wsdl";//
+            //https://pilotosiatservicios.impuestos.gob.bo/v2/FacturacionCodigos?wsdl
+            //string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJFbnpvIiwiY29kaWdvU2lzdGVtYSI6IjZEMjQ5Njk0MEFCMzk0QzU3MERBMTJGIiwibml0IjoiSDRzSUFBQUFBQUFBQURPMk1EQzB0REF6TURRSEFHNkxrVU1LQUFBQSIsImlkIjo2MjAwNDUsImV4cCI6MTY0MzU4NzIwMCwiaWF0IjoxNjQwMjkyMTI5LCJuaXREZWxlZ2FkbyI6MzgwMTk4NjAxNywic3Vic2lzdGVtYSI6IlNGRSJ9.WSJzg9BY7JvLRtApjO86880x8ezJUocNoVHf4U9BvhqujOsKUCy7sWb0Ypp_IgxHbHuios0dRCc9Grb6jPjRIw";
+            BasicHttpBinding binding = new BasicHttpBinding
+            {
+                SendTimeout = TimeSpan.FromSeconds(1000),
+                MaxBufferSize = int.MaxValue,
+                MaxReceivedMessageSize = int.MaxValue,
+                AllowCookies = true,
+                ReaderQuotas = XmlDictionaryReaderQuotas.Max
+            };
+            binding.Security.Mode = BasicHttpSecurityMode.Transport; // https   apikey: TokenApi   
+            //binding.Security.Mode = BasicHttpSecurityMode.None; // http
+            EndpointAddress address = new EndpointAddress(endpointAddress);
+
+            ServicioFacturacionCodigosClient servicio = new ServicioFacturacionCodigosClient(binding, address);
+
+            servicio.Endpoint.EndpointBehaviors.Add(new CustomAuthenticationBehaviour($"TokenApi {token}"));  // ($"apikey TokenApi {token}"))
+            //servicio.Open();
+            try
+            {
+                Task<verificarComunicacionResponse> resp = servicio.verificarComunicacionAsync();
+                //Task<verificarComunicacionResponse> resp = servicio.verificarComunicacionAsync();
+
+                resp.Wait();
+                var respuesta = resp.Result.RespuestaComunicacion.ToString();//resp.Result.RespuestaComunicacion.ToString();
+                MessageBox.Show(respuesta);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("el error es"+ ex.Message);
+            }
+
+        }
+    }
+}
